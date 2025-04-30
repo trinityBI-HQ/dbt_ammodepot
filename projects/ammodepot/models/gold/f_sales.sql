8cacb2ce3014329684af55df4a2ae70c7a272dfc
RicardoTrinityBI <ricardo@trinitybi.com>
2025-04-30 16:57:57 -0400

    cost_fishbowl_final cte updated

--- projects/ammodepot/models/gold/f_sales.sql
+++ projects/ammodepot/models/gold/f_sales.sql
@@ -166,17 +166,29 @@ cost_fishbowl_final AS (
 <<<<<<< HEAD
         COALESCE(NULLIF(b.cost,0), NULLIF(k.cost,0), fc.cost * b.qty) AS cost,
         b.totalcost AS totalcost
-=======
-        COALESCE(NULLIF(b.total_cost,0), NULLIF(k.cost,0)) AS cost,
-        b.total_cost as totalcost,
->>>>>>> b3cefdd (Fixing the costs)
         k.cost                            AS costbundle,
+        m.magento_order_item_identity     AS magento_order,
         m.magento_order_item_identity     AS magento_order,
         fc.cost                           AS costfiltered,
         pr.produto_magento                AS id_produto_magento,
         child.mgntid                      AS id_magento,,
+        pr.produto_magento                AS id_produto_magento,
+        child.mgntid                      AS id_magento,,
         b.so_item_id,
         b.sales_order_id,
+        ca.count_of_id_magento,
+        b.product_id                                              AS id_produto_fishbowl,
+        p.is_kit                                                  AS bundle,
+        COALESCE(k.costprocessing, a.averagecost)                 AS averageweightedcost,
+        b.scheduled_fulfillment_date                              AS scheduled_fulfillment_date,
+        b.quantity_fulfilled                                      AS qty
+    FROM {{ref('fishbowl_soitem')}}      AS b
+    LEFT JOIN conversion_soitem         AS child ON b.so_item_id       = child.idfb
+    LEFT JOIN product_avg_cost          AS a     ON b.product_id       = a.id_produto
+    LEFT JOIN conversion_product        AS pr    ON b.product_id       = pr.produtofish
+    LEFT JOIN magento_identities        AS m     ON b.sales_order_id   = m.code
+    LEFT JOIN cost_aggregation          AS ca    ON child.mgntid       = ca.id
+    FROM {{ ref('fishbowl_product') }}      AS p     ON b.product_id       = p.product_id
         ca.count_of_id_magento,
         b.product_id                                              AS id_produto_fishbowl,
         p.is_kit                                                  AS bundle,
